b728a4138b1f7c8c9d6ffac69e8ab4abaddf604b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-05 04:42:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,15 +16,15 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
-  # post "/signup" do
-  #   user = User.new(params)
-  #
-  #   if user.save
-  #     redirect '/login'
-  #   else
-  #     redirect '/failure'
-  #   end
-  # end
+  post "/signup" do
+    user = User.new(params)
+  
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
   #
   # get '/account' do
   #   @user = User.find(session[:user_id])
